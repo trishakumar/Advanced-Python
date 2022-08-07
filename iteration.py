4cf59f53b83eb47993c8cdabd20f1cdab1191aa7
trishakumar <58877381+trishakumar@users.noreply.github.com>
2022-08-07 15:26:59 +0530

    Add files via upload

---
+++ iteration.py
@@ -0,0 +1,26 @@
+class Topten:
+
+    def __init__(self):
+        self.num = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        if self.num <= 10:
+            val = self.num
+            self.num += 1
+
+            return val
+        else:
+            raise StopIteration
+
+
+
+values = Topten()
+
+print(next(values))
+
+for i in values:
+    print(i)
